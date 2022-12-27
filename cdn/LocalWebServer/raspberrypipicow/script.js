6f56119f6962de752e5ca643ebf9e16585a9a434
Komas19-new <benoitbourcier@outlook.com>
2022-12-27 18:17:31 +0100

    Update script.js

--- cdn/LocalWebServer/raspberrypipicow/script.js
+++ cdn/LocalWebServer/raspberrypipicow/script.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
-      // If the checkbox is checked, update the URL to "/lighton"
-      window.history.replaceState({}, '', '/lighton');
+      // If the checkbox is checked, update the URL to "/lighton?"
+      window.history.replaceState({}, '', '/lighton?');
     } else {
-      // If the checkbox is not checked, update the URL to "/lightoff"
-      window.history.replaceState({}, '', '/lightoff');
+      // If the checkbox is not checked, update the URL to "/lightoff?"
+      window.history.replaceState({}, '', '/lightoff?');
     }
   });
 });
