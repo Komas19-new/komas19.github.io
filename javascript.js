67949f4180111228f834251da2fc4533e8d96b5d
Komas19-new <benoitbourcier@outlook.com>
2023-05-02 17:25:47 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,24 +4,33 @@ document.addEventListener("DOMContentLoaded", function() {
   function copyBtn() { // For blog posts 
      // Select the entire blog post
       const blogPost = document.querySelector('.post');
+      console.log("Getting blog post");
       const range = document.createRange();
       range.selectNode(blogPost);
       window.getSelection().removeAllRanges();
       window.getSelection().addRange(range);
+      console.log("copy");
   
       // Copy the selected text to the clipboard
       navigator.clipboard.writeText(window.getSelection().toString());
+      console.log("clipboard");
   
       // Deselect the text
       window.getSelection().removeAllRanges();
+      console.log("deselect");
 
       // Show the copy alert
       copyAlert.style.opacity = 1;
+      console.log("opacity 1");
       copyAlert.style.visibility = "visible";
+      console.log("visible");
       setTimeout(function() {
         copyAlert.style.opacity = 0;
+        console.log("opacity 0");
         copyAlert.style.visibility = "hidden";
+        console.log("hidden");
       }, 500);
+      console.log("we're done");
     }
 
   function responsiveTopNav() { // For the Top Bar
