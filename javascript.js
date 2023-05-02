f38d8d49f961124cc86977e57b9981741bc5069b
Komas19-new <benoitbourcier@outlook.com>
2023-05-02 17:08:11 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
   var copyAlert = document.getElementById("copyalert");
 
-function copyButton() { // For blog posts 
-    // Select the entire blog post
-    const blogPost = document.querySelector('.post');
-    const range = document.createRange();
-    range.selectNode(blogPost);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
+  function copyButton() { // For blog posts 
+     // Select the entire blog post
+      const blogPost = document.querySelector('.post');
+      const range = document.createRange();
+      range.selectNode(blogPost);
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
   
-    // Copy the selected text to the clipboard
-    navigator.clipboard.writeText(window.getSelection().toString());
+      // Copy the selected text to the clipboard
+      navigator.clipboard.writeText(window.getSelection().toString());
   
-    // Deselect the text
-    window.getSelection().removeAllRanges();
+      // Deselect the text
+      window.getSelection().removeAllRanges();
 
-    // Show the copy alert
-    copyAlert.style.opacity = 1;
-    copyAlert.style.visibility = "visible";
-    setTimeout(function() {
-      copyAlert.style.opacity = 0;
-      copyAlert.style.visibility = "hidden";
-    }, 500);
-  }
-
-function responsiveTopNav() { // For the Top Bar
-    var x = document.getElementById("NavTop");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+      // Show the copy alert
+      copyAlert.style.opacity = 1;
+      copyAlert.style.visibility = "visible";
+      setTimeout(function() {
+        copyAlert.style.opacity = 0;
+        copyAlert.style.visibility = "hidden";
+      }, 500);
     }
-  }
-})
+
+  function responsiveTopNav() { // For the Top Bar
+      var x = document.getElementById("NavTop");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+  })
